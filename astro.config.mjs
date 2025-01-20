c1517937a686062814f3e5848f1cf76982b7d382
tmorin01 <tommorin95@gmail.com>
2025-01-20 18:06:47 -0500

    add about and blog pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://tmorin01.github.io',
-    base: 'astro_tutorial'
+    site: 'https://tmorin01.github.io'
 });
