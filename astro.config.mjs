7d0cd680eec3ba8d4040cb03ea2d8636b2d887e3
tmorin01 <tommorin95@gmail.com>
2025-01-20 18:09:18 -0500

    fix base in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://tmorin01.github.io'
+    site: 'https://tmorin01.github.io',
+    base: 'astro_tutorial'
 });
