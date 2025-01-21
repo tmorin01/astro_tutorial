a5420e529ce7265f67592f327763efd242fb4370
tmorin01 <tommorin95@gmail.com>
2025-01-20 21:51:57 -0500

    integrate with tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://tmorin01.github.io',
-    base: 'astro_tutorial'
+  site: 'https://tmorin01.github.io',
+  base: 'astro_tutorial',
+  integrations: [
+    tailwind({
+        configFile: './tailwind.config.mjs',
+    }),
+  ],
 });
